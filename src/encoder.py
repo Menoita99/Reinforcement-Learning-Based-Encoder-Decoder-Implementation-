61a3176a5697c8f0ed8b2329548077d2f1502246
Francisco Cardoso <60990445+Franciscodc98@users.noreply.github.com>
2021-04-24 13:50:38 +0100

    sd

--- src/encoder.py
+++ src/encoder.py
@@ -7,8 +7,8 @@ class Mlp(nn.Module):
     def __init__(self, input_dim, features_dim):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(input_dim,128),
-           # nn.BatchNorm1d(num_features=20),
+            nn.Linear(input_dim, 128),
+            # nn.BatchNorm1d(num_features=20),
             nn.Linear(128, features_dim),
         )
 
@@ -16,11 +16,9 @@ class Mlp(nn.Module):
         return self.net(input)
 
 
-
 class Cnn(nn.Module):
 
     def __init__(self, input_dim, output_dim):
-
         self.net = nn.Sequential(
             # TODO
         )
@@ -29,28 +27,22 @@ class Cnn(nn.Module):
             return self.net(input)
 
 
-
-
 class Gru(nn.Module):
 
     def __init__(self, input_dim, output_dim):
-
         self.net = nn.Sequential(
-            #TODO
+            # TODO
         )
 
         def forward(self, input):
             return self.net(input)
 
 
-
-
 class CnnGru(nn.Module):
 
     def __init__(self, input_dim, output_dim):
-
         self.net = nn.Sequential(
-            #TODO
+            # TODO
         )
 
         def forward(self, input):
